89dfdb8cc5a5dec76c06af4c47d37d4c9c04bd7b
uldana-baitiles <u_baitiles@kbtu.kz>
2026-01-25 18:37:34 +0500

    added examples for topic numbers

---
+++ Practice1/python-basics/07_ Numbers.py
@@ -0,0 +1,38 @@
+# Example 1
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+
+# Example 2
+x = 35e3    # float
+y = 12E3    # float
+z = -87.7e100  # float
+
+
+# Example 3
+#Import the random module, and display a random number from 1 to 9:
+import random
+print(random.randrange(1, 10))
+
+
+# Example 4
+#int
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# Example 5
+#complex
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
