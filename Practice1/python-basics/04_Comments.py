168131eadefe178a865a8722a84de286f69ac85c
uldana-baitiles <u_baitiles@kbtu.kz>
2026-01-25 17:50:31 +0500

    added examples for topic comments

---
+++ Practice1/python-basics/04_Comments.py
@@ -0,0 +1,21 @@
+# Example 1
+a = 7
+b = 4
+print("Sum:", a + b)
+# Example 2
+print("Hello, World!") #This is a comment
+# Example 3
+#print("Hello, World!")
+print("HELLO")
+# Example 4
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+# Example 5
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
