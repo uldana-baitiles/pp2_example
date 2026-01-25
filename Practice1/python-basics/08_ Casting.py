5d6eef52372640faca9d050ca216fbc11ace4876
uldana-baitiles <u_baitiles@kbtu.kz>
2026-01-25 18:43:53 +0500

    added examples for topic  casting

---
+++ Practice1/python-basics/08_ Casting.py
@@ -0,0 +1,34 @@
+# Example 1 Integers:
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+
+# Example 2 Floats:
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+
+# Example 3 Strings:
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
+
+
+# Example 4
+a = int("12")     # int
+b = float(7)      # float
+c = str(3.14)     # str
+
+print(a, b, c)
+
+
+# Example 5
+p = int(4)
+q = float(2)
+r = str("hello")
+
+print(p, q, r)
+
