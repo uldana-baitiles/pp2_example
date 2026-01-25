0c2c0ada27910458fb1513a95d3b182267e4a885
uldana-baitiles <u_baitiles@kbtu.kz>
2026-01-25 17:38:46 +0500

    added examples for topic sytax

---
+++ Practice1/python-basics/Syntax.py
@@ -0,0 +1,17 @@
+# Example 1
+print("Python is fun!")
+# Example 2
+if 5 > 2:
+  print("Five is greater than two!")
+# Example 3
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!")
+# Example 4
+print("Hello"); print("How are you?"); print("Bye bye!")
+# Example 5
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
