809eac22b1aeb36c8fe128cf7335dd9ac68c495e
uldana-baitiles <u_baitiles@kbtu.kz>
2026-01-25 18:29:16 +0500

    added examples for topic data types

---
+++ Practice1/python-basics/06_Data Types.py
@@ -0,0 +1,39 @@
+# Example 1
+x = 20.5
+print(x)
+print(type(x)) 
+#(20.5
+#<class 'float'>)
+
+
+# Example 2
+x = 1j
+print(x)
+print(type(x)) 
+#(lj
+#<class 'complex'>)
+
+
+# Example 3
+x = ["apple", "banana", "cherry"]
+print(x)
+print(type(x))  
+#(l['apple', 'banana', 'cherry']
+#<class 'list'>)
+
+
+# Example 4
+x = {"name" : "John", "age" : 36}
+print(x)
+print(type(x)) 
+#({'name': 'John', 'age': 36}
+#<class 'dict'>)
+
+
+# Example 5
+x = True
+print(x)
+print(type(x)) 
+#(True
+#<class 'bool'>)
+
