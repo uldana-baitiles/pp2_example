6462887fbb9249a46769453cb6d64e8e5c42074c
uldana-baitiles <u_baitiles@kbtu.kz>
2026-02-03 22:17:08 +0500

    addded examples for loop

---
+++ Practice2/loops/04_for_loop.py
@@ -0,0 +1,27 @@
+# Example 1:  
+for i in range(5):
+    print(i)
+
+
+# Example 2:  
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+
+# Example 3:  
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+for x in adj:
+    for y in fruits:
+        print(x, y)
+
+# Example 4: 
+word = "Python"
+for letter in word:
+    print(letter)
+
+
+# Example 5:  
+for x in [0, 1, 2]:
+  pass
