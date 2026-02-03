25658a616249dd6819c7c7ae56bafb93a12c6de9
uldana-baitiles <u_baitiles@kbtu.kz>
2026-02-03 21:58:12 +0500

    added examples for while loop

---
+++ Practice2/loops/01_while_loop.py
@@ -0,0 +1,42 @@
+# Example 1:  
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+
+# Example 2: S 
+total = 0
+number = 1
+
+while number <= 3:
+    total += number
+    number += 1
+
+print("Sum:", total)
+
+
+# Example 3: 
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+
+
+# Example 4:  
+age = 32
+# The test condition is always True
+while age > 18:
+    print('You can vote')
+
+
+# Example 5:  
+age = 16
+
+while age < 18:
+    print("Too young")
+    age += 1
+
+print("Now you are 18")
