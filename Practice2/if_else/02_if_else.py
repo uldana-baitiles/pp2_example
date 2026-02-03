bf2f8c2071938ab70a7395f02e755eec2c6fd347
uldana-baitiles <u_baitiles@kbtu.kz>
2026-02-03 21:40:40 +0500

    examples for if else

---
+++ Practice2/if_else/02_if_else.py
@@ -0,0 +1,41 @@
+# Example 1
+
+a = 5
+b = 10
+if a > b:
+    print("a is greater than b")
+else:
+    print("a is less than or equal to b")
+
+
+# Example 2
+number = int(input("Enter a number: "))
+if number >= 0:
+    print("Number is zero or positive")
+else:
+    print("Number is negative")
+
+
+# Example 3
+word = "Python"
+if len(word) > 5:
+    print("The word is long")
+else:
+    print("The word is short")
+
+
+# Example 4
+
+is_logged_in = False
+if is_logged_in:
+    print("User is logged in")
+else:
+    print("User is not logged in")
+
+
+# Example 5
+numbers = [1, 2, 3, 4, 5]
+if 3 in numbers:
+    print("3 is in the list")
+else:
+    print("3 is not in the list")
