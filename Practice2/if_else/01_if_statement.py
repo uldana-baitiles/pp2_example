6165b0926fc1a2c6aa9e766b77dc2e585acca596
uldana-baitiles <u_baitiles@kbtu.kz>
2026-02-03 21:35:03 +0500

    examples for if statement

---
+++ Practice2/if_else/01_if_statement.py
@@ -0,0 +1,33 @@
+# Example 1
+a = 33
+b = 200
+if b > a:
+    print("b is greater than a")
+
+
+# Example 2
+is_logged_in = True
+if is_logged_in:
+  print("Welcome back!")
+
+
+# Example 3
+
+number = -5
+if number > 0:
+    print("this will not be printed")
+
+
+# Example 4
+
+x = 5
+y = 5
+if x == y:
+    print("x is equal to y")
+
+
+# Example 5
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
