65a61ca1d99f43ac89023e3cd786ffd74a07db3c
uldana-baitiles <u_baitiles@kbtu.kz>
2026-02-10 16:28:39 +0500

    added examples for init method

---
+++ Practice3/03_classes/02_init_method.py
@@ -0,0 +1,43 @@
+#Example 1
+# The __init__() method is called automatically every time 
+# the class is being used to create a new object.
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Emil", 36)
+
+print(p1.name)
+print(p1.age)
+
+#Example 2 Default Values in __init __()
+class Person:
+  def __init__(self, name, age=18):#age is default
+    self.name = name
+    self.age = age
+
+p1 = Person("Emil")
+p2 = Person("Tobias", 25)
+
+print(p1.name, p1.age)
+print(p2.name, p2.age)
+
+
+#Example 3 Multiple Parameters in __init __()
+
+class Person:
+    def __init__(self, name, age, city, country):
+        self.name = name
+        self.age = age
+        self.city = city
+        self.country = country
+
+p1 = Person("Linus", 30, "Oslo", "Norway")
+
+print(p1.name)
+print(p1.age)
+print(p1.city)
+print(p1.country)
+
