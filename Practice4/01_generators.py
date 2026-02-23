e9bdfb45667a88584d345e70f0c48a3cf92244eb
uldana-baitiles <u_baitiles@kbtu.kz>
2026-02-23 16:51:55 +0500

    completed the task 1 in generators

--- Practice4/01_generators.py
+++ Practice4/01_generators.py
@@ -1,1 +1,9 @@
 #task1
+#Create a generator that generates the squares of numbers up to some number N
+def sq(n):
+    for i in range (1,n+1):
+        yield i*i
+n=int(input())
+
+for i in sq(n):
+    print(i,end=" ")
