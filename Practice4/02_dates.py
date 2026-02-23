01ec6126ff499a8b71974f2f973f3065e0cfbf3d
uldana-baitiles <u_baitiles@kbtu.kz>
2026-02-23 18:18:43 +0500

    competed task1 for dates

---
+++ Practice4/02_dates.py
@@ -0,0 +1,8 @@
+#task1
+#Write a Python program to subtract five days from current date
+"""
+from datetime import datetime,timedelta
+today=datetime.now()
+new=today-timedelta(days=5)
+print(new)
+"""
