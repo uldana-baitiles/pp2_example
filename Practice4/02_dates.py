d05700eabb78646100e6c74f93d8086f7eba8610
uldana-baitiles <u_baitiles@kbtu.kz>
2026-02-23 18:23:53 +0500

    competed task2 for dates

--- Practice4/02_dates.py
+++ Practice4/02_dates.py
@@ -6,3 +6,19 @@ today=datetime.now()
 new=today-timedelta(days=5)
 print(new)
 """
+
+
+#task2
+#Write a Python program to print yesterday, today, tomorrow.
+"""
+from datetime import datetime,timedelta
+today=datetime.now()
+yesterday=today-timedelta(days=1)
+tomorrow=today+timedelta(days=1)
+print("Yesterday:", yesterday)
+print("Today:", today)
+print("Tomorrow:", tomorrow)
+
+"""
+
+
