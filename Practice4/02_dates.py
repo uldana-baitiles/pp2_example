c4d56df5c9508829493bdeebe270af441b4fd6c0
uldana-baitiles <u_baitiles@kbtu.kz>
2026-02-23 19:05:14 +0500

    competed task 4 for datees

--- Practice4/02_dates.py
+++ Practice4/02_dates.py
@@ -22,3 +22,25 @@ print("Tomorrow:", tomorrow)
 """
 
 
+#task3
+#Write a Python program to drop microseconds from datetime.
+"""
+from datetime import datetime
+now=datetime.now()
+a=now.replace(microsecond=0)
+print(a)
+"""
+
+
+
+#task4
+#Write a Python program to calculate two date difference in seconds
+"""
+from datetime import datetime 
+date1=datetime(2026,2,23,12,0,0)
+date2=datetime(2026,2,20,2,30,0)
+diff=abs(date1-date2)
+seconds=diff.total_seconds()
+print(seconds)
+
+"""
